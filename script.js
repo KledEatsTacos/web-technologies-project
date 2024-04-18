d258ee508faa36d59e7cb3f26ed826a0e12d8e42
Mustafa Masri <mustafamasry180@gmail.com>
2024-04-18 15:40:26 +0300

    removed unnecessary code

--- script.js
+++
@@ -1,9 +0,0 @@
-var navbarCollapse = document.getElementById('navbarNavDropdown');
-
-navbarCollapse.addEventListener('show.bs.collapse', function() {
-    document.getElementById('construction').classList.add('hidden');
-});
-
-navbarCollapse.addEventListener('hidden.bs.collapse', function() {
-    document.getElementById('construction').classList.remove('hidden');
-});
