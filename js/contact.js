57f4b9632c190125f4c5cce21c4bfd33c499d459
Mustafa Masri <mustafamasry180@gmail.com>
2024-05-16 22:58:34 +0300

    javascript + vue.js submit buttons

--- js/contact.js
+++ js/contact.js
@@ -1,33 +1,15 @@
-new Vue({
-    el: '#app',
-    data: {
-        name: '',
-        email: '',
-        message: ''
-    },
-    methods: {
-        validateEmail: function(email) {
-            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(String(email).toLowerCase());
-        },
-        submitForm: function(event) {
-            if (!this.name || !this.email || !this.message) {
-                alert('All fields are required.');
-                return;
-            }
+function submitWithJS() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
 
-            if (!this.validateEmail(this.email)) {
-                alert('Please enter a valid email address.');
-                return;
-            }
+    var formData = {
+        name: name,
+        email: email,
+        message: message
+    };
 
-            localStorage.setItem('formData', JSON.stringify({ name: this.name, email: this.email, message: this.message }));
-            window.location.href = 'form-data.html';
-        },
-        resetForm: function() {
-            this.name = '';
-            this.email = '';
-            this.message = '';
-        }
-    }
-});
+    localStorage.setItem('formDataJS', JSON.stringify(formData));
+
+    window.location.href = 'form-data.html';
+}
