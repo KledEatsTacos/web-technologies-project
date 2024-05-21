0d2de34259924d6d36a5f48265177f1a6342d546
Mustafa Masri <mustafamasry180@gmail.com>
2024-05-21 19:12:44 +0300

    credential validation for login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,4 @@
-function Back() {
-  window.history.back();
-}
+
 
 //on a successful login
 localStorage.setItem('isLoggedIn', 'true');
--- login-successful.php
+++ login-successful.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
-
 $correct_username = "g221210580@sakarya.edu.tr";
 $correct_password = "g221210580";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["email"];
     $password = $_POST["password"];
-
     if (!empty($username) && !empty($password) && filter_var($username, FILTER_VALIDATE_EMAIL)) {
         if ($username == $correct_username && $password == $correct_password) {
             $_SESSION['loggedin'] = true; 
@@ -22,10 +19,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </html>";
             header("refresh:2;url=index.html"); //2 seconds is good enough
         } else {
-            header("Location: login.html");
+            header("Location: login-failed.html");
         }
     } else {
-        header("Location: login.html");
+        header("Location: login-failed.html");
     }
 }
 ?>
